60e1df350d1f8721394973084ca1ac44039afe84
aka <akatsukisatoshi@gmail.com>
2022-03-20 11:53:06 +0900

    詳細ページを追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
+import DetailsBook from "../pages/DetailsBook.svelte";
 import SearchBook from "../pages/SearchBook.svelte";
 
 export const routes = {
 	"/": SearchBook,
+	"/books/:id": DetailsBook,
 };
